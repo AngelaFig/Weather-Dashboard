af3dd75a792e86b398e90940417d1227a4226da0
AngelaFig <angelafigueroa596@gmail.com>
2023-07-08 19:13:45 -0400

    Added intial HTML for nav, search bar, and search history section.  Added api key and api call format tto js. Added some basic style overrides to css

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+// US CODE: 840 
+
+var apiKey = '59939906650b56969f54c525743fa617'; 
+
